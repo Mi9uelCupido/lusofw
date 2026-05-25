9f47dc52e59cf24804f9398f31fbaa356f128b63
João Brázio <jbrazio@gmail.com>
2026-05-25 08:39:44 +0000

    v2026.5.2

--- examples/simple_repeater/MyMesh.h
+++ examples/simple_repeater/MyMesh.h
@@ -88,7 +88,7 @@ struct TimeSample {
 #endif
 
 #ifndef LUSOFW_FIRMWARE_VERSION
-  #define LUSOFW_FIRMWARE_VERSION "2026.5.1"
+  #define LUSOFW_FIRMWARE_VERSION "2026.5.2"
 #endif
 
 #define FIRMWARE_ROLE "repeater"
